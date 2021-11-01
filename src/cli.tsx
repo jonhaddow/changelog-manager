1668bf590996a733ffe4de0772c2e810c237ee6e
Jon Haddow <jon@haddow.me>
2021-11-01 19:47:38 +0000

    Simplify API for specifying a severity

--- src/cli.tsx
+++ src/cli.tsx
@@ -1,9 +1,8 @@
 import * as React from "react";
 import { render } from "ink";
 
-import { Command, Option } from "commander";
+import { Command } from "commander";
 import { Add, List, Release } from "./commands";
-import { Flags } from "./common";
 
 const program = new Command("changelog-manager");
 
@@ -11,17 +10,29 @@ const add = program.command("add");
 
 add
 	.description("Adds a pending changelog entry")
-	.addOption(
-		new Option("-t, --type <type>", "The type of the change")
-			.choices(["patch", "minor", "major"])
-			.default("patch")
-	)
+	.option("--patch", "For a bug fix (default)")
+	.option("--minor", "This change is for new functionality")
+	.option("--major", "This change is a breaking change")
 	.requiredOption("-m, --message <msg>", "The message describing the change")
 	.option("-g, --group <group>", "Group with other changes")
 	.showHelpAfterError()
-	.action((args: Flags) => {
-		render(<Add type={args.type} message={args.message} group={args.group} />);
-	});
+	.action(
+		(args: {
+			patch?: boolean;
+			minor?: boolean;
+			major?: boolean;
+			message: string;
+			group?: string;
+		}) => {
+			render(
+				<Add
+					type={args.major ? "major" : args.minor ? "minor" : "patch"}
+					message={args.message}
+					group={args.group}
+				/>
+			);
+		}
+	);
 
 const list = program.command("list");
 list.description("Lists all pending changelog entries").action(() => {
